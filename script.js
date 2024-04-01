943cc60abae8dfa27aa0721fff20c9e0cc8218cb
Sravyareddy04 <162128264+Sravyareddy04@users.noreply.github.com>
2024-04-01 15:01:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function searchWeather() {
+    const apiKey = '7f7f91d5fc69a4912a7081b7958fb3f2';
+    const city = document.getElementById('cityInput').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherResult = document.getElementById('weatherResult');
+            weatherResult.innerHTML = `
+                <h2>Weather in ${data.name}</h2>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
